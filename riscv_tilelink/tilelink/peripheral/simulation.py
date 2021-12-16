55b1c671b9783ec21cd52961b530237330bf7c7b
Michiel Visser <opensource@webmichiel.nl>
2021-12-16 20:27:57 +0100

    Remove the cycle counter from the simulation peripheral
    The RISC-V core supports the `cycles` and `instret` CSR, therefore this
    counter is no longer needed.

--- riscv_tilelink/tilelink/peripheral/simulation.py
+++ riscv_tilelink/tilelink/peripheral/simulation.py
@@ -27,10 +27,6 @@ class TilelinkSimulationPeripheral(Elaboratable):
         # Automatically reset the register to zero
         m.d.sync += self.sim_output_valid.eq(False)
 
-        # Cycle counter
-        cycle_count = Signal(unsigned(64))
-        m.d.sync += cycle_count.eq(cycle_count + 1)
-
         # Does the A bus fire in this cycle
         a_fire = self.bus.a.valid & self.bus.a.ready
         we = self.bus.a.opcode.matches(tilelink.ChannelAOpcode.PutFullData, tilelink.ChannelAOpcode.PutPartialData)
@@ -46,10 +42,6 @@ class TilelinkSimulationPeripheral(Elaboratable):
                     with m.Case(4):
                         m.d.sync += self.halt_simulator.word_select(i, 8).eq(self.bus.a.data.word_select(i, 8))
                     with m.Case(8):
-                        m.d.sync += cycle_count[0:32].word_select(i, 8).eq(self.bus.a.data.word_select(i, 8))
-                    with m.Case(12):
-                        m.d.sync += cycle_count[32:64].word_select(i, 8).eq(self.bus.a.data.word_select(i, 8))
-                    with m.Case(16):
                         m.d.sync += self.external_interrupt.word_select(i, 8).eq(self.bus.a.data.word_select(i, 8))
 
         # Determine the ready of the input bus
@@ -68,10 +60,6 @@ class TilelinkSimulationPeripheral(Elaboratable):
                     with m.Case(4):
                         m.d.sync += self.bus.tilelink_access_ack_data(data=self.halt_simulator, size=self.bus.a.size, source=self.bus.a.source)
                     with m.Case(8):
-                        m.d.sync += self.bus.tilelink_access_ack_data(data=cycle_count[0:32], size=self.bus.a.size, source=self.bus.a.source)
-                    with m.Case(12):
-                        m.d.sync += self.bus.tilelink_access_ack_data(data=cycle_count[32:64], size=self.bus.a.size, source=self.bus.a.source)
-                    with m.Case(16):
                         m.d.sync += self.bus.tilelink_access_ack_data(data=self.external_interrupt, size=self.bus.a.size, source=self.bus.a.source)
             with m.Case(tilelink.ChannelAOpcode.PutFullData, tilelink.ChannelAOpcode.PutPartialData):
                 m.d.sync += self.bus.tilelink_access_ack(size=self.bus.a.size, source=self.bus.a.source)
